af99387967eaa8f65ae7d1106b4b6f8e3ff2da8e
Donquixot <64096633+Donquixot228@users.noreply.github.com>
2022-12-21 15:38:20 +0200

    Update at_base_response.dart

--- lib/at_base_response.dart
+++ lib/at_base_response.dart
@@ -2,6 +2,6 @@
 class BaseResponse {
   final String requestMessage;
   final String placementID;
-final bool? isDeeplinkSuccess;
+  final bool? isDeeplinkSuccess;
   BaseResponse(this.requestMessage,this.placementID,this.isDeeplinkSuccess);
 }
